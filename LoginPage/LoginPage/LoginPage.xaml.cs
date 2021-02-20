34e578946a5675fd90b1b5055d6c8b2517a77b5c
Osv04 <oscar.veloz4@gmail.com>
2021-02-19 23:54:33 -0400

    App first commit
    App

---
+++ LoginPage/LoginPage/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private async void BtnLogin(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Email.Text) || string.IsNullOrEmpty(Password.Text))
+            {
+                await DisplayAlert("Error", "Valores Nulos", "Cancel");
+            }
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
